5751ff828fc838b8e587adc9a810b8370b753ddc
hyps4310 <37211517+hyps4310@users.noreply.github.com>
2025-05-30 21:27:41 +0000

    my first action commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # custom_action
 A repo specifically for a custom action
+
+# Hello world javascript action
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+```yaml
+uses: actions/hello-world-javascript-action@e76147da8e5c81eaf017dede5645551d4b94427b
+with:
+  name: 'Mona the Octocat'
+```
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import * as core from '@actions/core';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-const name = core.getInput('name');
-const ouput_value = `Hello ${name} from custom action!`;
-core.setOutput('greeting', ouput_value);
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
